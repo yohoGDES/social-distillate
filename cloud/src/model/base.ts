af4ba87ee6582049cd551407b2267ef32d5e09ee
Heath Padrick <heath.padrick@gmail.com>
2021-10-21 21:43:47 -0400

    removed unneeded abstract class

--- cloud/src/model/base.ts
+++ cloud/src/model/base.ts
@@ -1,8 +1,8 @@
 import Parse from 'parse'
 
 abstract class Base<T> extends Parse.Object {
-    constructor(modelName: string) {
-        super(modelName)
+    constructor(modelName: string, init?: Partial<T>) {
+        super(modelName, init)
 
         return new Proxy(this, {
             set(target: Base<T>, prop: keyof Base<T>, value: any) {
@@ -19,23 +19,14 @@ abstract class Base<T> extends Parse.Object {
     }
 }
 
-abstract class BaseModel<T> extends Base<T> {
-    constructor(modelName: string, init?: Partial<T>) {
-        super(modelName)
-        if(init) {
-            Object.assign(this, init)
-        }
-    }
-} 
-
 interface BaseAttributes {
     id: string
     updatedAt: Date
     createdAt: Date
 }
 
-export function expose<T extends Base<T>>(modelClass: new (init?: Partial<T>) => T) {
-    type Attributes = BaseAttributes & Omit<T, keyof Parse.Object>
+export function expose<T extends Base<T>, P = Parse.Object>(modelClass: new (init?: Partial<T>) => T) {
+    type Attributes = BaseAttributes & Omit<T, keyof P>
     class Model {
         constructor(init?: Partial<T>) {
             return new modelClass(init)
@@ -44,4 +35,4 @@ export function expose<T extends Base<T>>(modelClass: new (init?: Partial<T>) =>
     return Model as new (init?: Partial<T>) => Attributes
 }
 
-export default BaseModel
+export default Base
