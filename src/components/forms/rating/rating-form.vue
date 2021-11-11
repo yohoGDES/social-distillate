c1094fd8f177d045444e7b4a071847666d9b8337
yohoGDES <yoho@yohodesign.com>
2021-11-10 20:03:33 -0500

    Rating form, tasting setup

---
+++ src/components/forms/rating/rating-form.vue
@@ -0,0 +1,232 @@
+<template>
+  <h2>Review #1</h2>
+  <div>Information about this tasting is hidden until the host reveals.</div>
+  <hr />
+  <form>
+    <sc-form-row>
+      <sc-form-label>Color</sc-form-label>
+      <sc-form-description>
+        Select the color that most closely matches the appearance of the liquid.
+      </sc-form-description>
+      <colors :color="review.color" @update:color="review.color = $event" />
+    </sc-form-row>
+
+    <sc-form-row>
+      <notes
+        v-model="review.nose"
+        label="Nose"
+        description="Why we nose goes here."
+      />
+    </sc-form-row>
+
+    <sc-form-row>
+      <notes
+        v-model="review.palate"
+        label="Palate"
+        description="What do we mean by palate here."
+      />
+    </sc-form-row>
+
+    <sc-form-row>
+      <notes
+        v-model="review.finish"
+        label="Finish"
+        description="What is in a finish?"
+      />
+    </sc-form-row>
+
+    <sc-form-row>
+      <sc-form-label>Value</sc-form-label>
+      <sc-form-description>
+        How much would you pay for this bottle?
+      </sc-form-description>
+      <sc-currency
+        v-model="review.value"
+        :options="{
+          currency: 'USD',
+          useGrouping: true
+        }"
+      />
+    </sc-form-row>
+
+    <sc-form-row>
+      <sc-form-label>Rating</sc-form-label>
+      <sc-form-description>
+        From 0 to 100 how would you rate this bottle?
+      </sc-form-description>
+      <sc-dropdown v-model="review.rating" :options="ratingScale" />
+    </sc-form-row>
+
+    <sc-form-row>
+      <sc-form-label>Flavor Profile</sc-form-label>
+      <flavor-wheel v-model="review.flavorProfile" />
+    </sc-form-row>
+
+    <sc-form-row>
+      <sc-form-label>Conclusion</sc-form-label>
+      <sc-form-description>
+        What did you think about this bottle overall?
+      </sc-form-description>
+      <notes v-model="review.conclusion" :hideSuggestions="true" />
+    </sc-form-row>
+
+    <sc-form-row>
+      <sc-button
+        rank="primary"
+        width="full"
+        type="button"
+        @clicked="submitReview"
+      >
+        Done
+      </sc-button>
+      <sc-button
+        rank="primary"
+        width="full"
+        type="button"
+        @clicked="testIt"
+      >
+        TEST
+      </sc-button>
+    </sc-form-row>
+  </form>
+</template>
+<script lang="ts">
+import colors from '@/components/forms/inputs/rate/colors.vue'
+import notes from '@/components/forms/inputs/rate/notes.vue'
+import flavorWheel from '@/components/flavor-wheel/flavor-wheel.vue'
+import { computed, defineComponent, reactive } from 'vue'
+import { TastingNotes } from '@/types'
+import { setPointer, setRelation } from '@/utilities/api'
+
+import { useRatingStore } from '@/store/modules/rate'
+import { useUserStore } from '@/store/modules/user'
+import { UserModel } from 'cloud/src/model/user'
+import { useBeverageStore } from '@/store/modules/beverage'
+
+export default defineComponent({
+  name: 'Rate',
+  components: {
+    notes,
+    colors,
+    flavorWheel
+  },
+  props: {
+    beverageId: {
+      type: String
+    }
+  },
+  emits: ['rating-complete'],
+  setup(props, { emit }) {
+    const userStore = useUserStore()
+    const ratingStore = useRatingStore()
+    const beverageStore = useBeverageStore()
+    const ratingScale = computed(() => [...Array(101).keys()].slice().reverse())
+
+    // TODO: Make setting up flavorProfiles dynamic based on a variable. We will want to load different sets of profiles for wine vs whiskey vs gin etc
+    const review: TastingNotes = reactive({
+      color: 'Amber',
+      nose: '',
+      palate: '',
+      finish: '',
+      rating: 0,
+      value: 0,
+      conclusion: '',
+      flavorProfile: [
+        {
+          key: 'smoky',
+          name: 'Smoky',
+          value: 0
+        },
+        {
+          key: 'peaty',
+          name: 'Peaty',
+          value: 0
+        },
+        {
+          key: 'herbal',
+          name: 'Herbal',
+          value: 0
+        },
+        {
+          key: 'oily',
+          name: 'Oily',
+          value: 0
+        },
+        {
+          key: 'sweet',
+          name: 'Sweet',
+          value: 0
+        },
+        {
+          key: 'briny',
+          name: 'Briny',
+          value: 0
+        },
+        {
+          key: 'salty',
+          name: 'Salty',
+          value: 0
+        },
+        {
+          key: 'vanilla',
+          name: 'Vanilla',
+          value: 0
+        },
+        {
+          key: 'tart',
+          name: 'Tart',
+          value: 0
+        },
+        {
+          key: 'fruity',
+          name: 'Fruity',
+          value: 0
+        },
+        {
+          key: 'floral',
+          name: 'Floral',
+          value: 0
+        },
+        {
+          key: 'sherried',
+          name: 'Sherried',
+          value: 0
+        },
+        {
+          key: 'chocolate',
+          name: 'Chocolate',
+          value: 0
+        },
+        {
+          key: 'cereal',
+          name: 'Cereal',
+          value: 0
+        }
+      ]
+    })
+
+    const testIt = () => emit('rating-complete', props.beverageId)
+    // const getBeverage = async () => {}
+    const submitReview = async () => {
+      const user = userStore.currentUser as UserModel
+      console.log('beverage ID', props.beverageId)
+      
+      // TODO: Get the beverage from a meta value
+      const rating = {
+        createdBy: setPointer(user.id as string, '_User'),
+        beverage: setRelation('LLUBGjqOxx', 'Beverage'),
+        ...review
+      }
+      const result = await ratingStore.saveRating(rating)
+      console.log('result', result)
+
+    }
+    return {
+      review,
+      ratingScale,
+      submitReview,
+      testIt
+    }
+  }
+})
+</script>
--- src/views/Tasting/Tasting.vue
+++ src/views/Tasting/Tasting.vue
@@ -1,5 +1,5 @@
 <template>
-<!-- 
+  <!-- 
   Users can create events
 Users can assign themselves as hosts and add co-hosts
 Users can create two event types: Tasting or Special Event
@@ -10,7 +10,7 @@ Hosts can choose from pre-entered bottles or enter a new one on the fly.
 Only the host and admin will be able to see blind tastings.
 The host controls the flow in the app. Once next dram is selected, users will receive the next scoring sheet.
 -->
-  Host (user)
+  <!-- Host (user)
   Co-Hosts (user)
   Group (relation: group)
   Location (saved user location or entered address)
@@ -33,5 +33,109 @@ host
   group
   location
   date
-  blind
+  blind -->
+  <div v-if="!isEmpty(tasting)">
+    Lets do this
+    <div v-if="userIsHost">
+      Hosting controls:
+      <sc-button>Begin Tasting</sc-button>
+      <sc-button>Next Beverage</sc-button>
+      <sc-button v-if="tasting.blind && completionPercentage === 100"
+        >Reveal Beverages</sc-button
+      >
+    </div>
+
+    <div
+      class="tasting-beverage"
+      v-for="(beverage, index) in tasting.beverages"
+      :key="beverage.index"
+    >
+      <rating-form
+        :beverageId="beverage.objectId"
+        @rating-complete="logRating($event)"
+        v-if="
+          !ratingIsCompleted(beverage.objectId) &&
+          index === completedRatings.length
+        "
+      />
+    </div>
+    <div class="progress-bar">
+      <div
+        class="progress-bar__percentage"
+        :style="`background-color: purple; width: ${completionPercentage}%`"
+      />
+      {{ ratingProgress }}
+    </div>
+    <div v-if="completionPercentage === 100">Done! Summary</div>
+  </div>
 </template>
+<script lang="ts">
+import { computed, defineComponent, onMounted, ref } from 'vue'
+import ratingForm from '@/components/forms/rating/rating-form.vue'
+import { useEventStore } from '@/store/modules/events'
+import { useRoute } from 'vue-router'
+import { isEmpty } from 'lodash'
+import { useUserStore } from '@/store/modules/user'
+
+export default defineComponent({
+  name: 'tasting',
+  components: { ratingForm },
+  setup() {
+    const eventStore = useEventStore()
+    const route = useRoute()
+    const tasting = ref()
+    const userStore = useUserStore()
+
+    const currentUser = computed(() => userStore.currentUser)
+    const userIsHost = computed(() => {
+      return tasting.value.host[0].objectId === currentUser.value?.id
+    })
+
+    const getEvent = async () => {
+      const event = await eventStore.getEvent(route.params.id as string)
+      tasting.value = event
+    }
+
+    const completedRatings = ref([] as string[])
+    const logRating = (beverage: string) => {
+      completedRatings.value.push(beverage)
+    }
+    const ratingIsCompleted = (id: string) =>
+      completedRatings.value.includes(id)
+    const ratingProgress = computed(() => {
+      return `${completedRatings.value.length} of ${tasting.value.beverages.length}`
+    })
+    const completionPercentage = computed(() => {
+      return (
+        (100 * completedRatings.value.length) / tasting.value.beverages.length
+      )
+    })
+
+    onMounted(async () => {
+      await getEvent()
+    })
+
+    return {
+      isEmpty,
+      userIsHost,
+      tasting,
+      logRating,
+      ratingIsCompleted,
+      ratingProgress,
+      completedRatings,
+      completionPercentage
+    }
+  }
+})
+</script>
+<style lang="scss">
+.progress-bar {
+  width: 100%;
+  display: block;
+}
+.progress-bar__percentage {
+  height: 3px;
+  display: block;
+  width: 0%;
+}
+</style>
